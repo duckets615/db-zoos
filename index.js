e2726ab67cb7e4d8510cc8b22dc58dcd8817c041
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-26 16:26:53 -0400

    init commit, packages, server setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const helmet = require('helmet');
 
+const server = express();
+
+server.use(express.json());
+server.use(helmet())
+
+
+const port = 8000;
+ server.listen(port, () => console.log(`server rolling on port ${port}`));
