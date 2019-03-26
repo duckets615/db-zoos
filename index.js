ddfa48715c183393e0ede03b98a8ebabdf7edd0f
duckets615 <33557104+duckets615@users.noreply.github.com>
2019-03-26 16:45:45 -0400

    post

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ server.use(helmet())
 const noId = `Id not found`
 
   // GET zoo list
-  router.get('/', (req, res)=>{
+  server.get('/', (req, res)=>{
     zoos.find()
       .then(zoos =>{
         res.status(200).json(zoos);
@@ -19,23 +19,8 @@ const noId = `Id not found`
       .catch(err => res.status(500).json(err));
   });
 
-  // GET zoo Id
-  router.get('/:id', async (req, res)=>{
-    try{
-      const {id} = req.params;
-      const zoo = await zoos.findById(id);
-      if(zoo){
-        res.status(200).json(zoo);
-      }else{
-        res.status(404).json(noId);
-      }
-    } catch(err) {
-      res.status(500).json(err);
-    }
-  });
-
-  // CRATE zoo
-  router.post('/', (req, res)=>{
+  // CREATE zoo
+  server.post('/', (req, res)=>{
     const zoo = req.body;
     zoos.add(zoo)
         .then(ids =>{
@@ -46,36 +31,6 @@ const noId = `Id not found`
       });
   });
 
-  // UPDATE zoo
-  router.put('/:id', (req, res)=>{
-    const {id} = req.params;
-    const changes = req.body;
-    zoos.update(id, changes)
-      .then(count =>{
-        if(count>0){
-          res.status(200).json(count);
-        }else{
-          res.status(404).json('ID not found.');
-        }
-      })
-      .catch(err => res.status(500).json(err));
-  });
-
-  // DELETE zoo
-  router.delete('/:id', (req, res)=>{
-    const {id} = req.params;
-    zoos.remove(id)
-      .then(count =>{
-        if(count>0){
-          res.status(200).json(count);
-        }else{
-          res.status(404).json(noId);
-        }
-      })
-      .catch(err => res.status(500).json(err));
-  })
-
-module.exports = router;
 
 
 
